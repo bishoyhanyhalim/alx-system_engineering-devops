5a550126b001c37be2f4f1b7009243378a64c0d8
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-04-21 16:46:36 -0700

    taks 100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,17 @@
+# clint config file pubbet
+
+include stdlib
+
+File_line { 'Turn off passwd auth':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no',
+  replace => true,
+}
+
+File_line { 'Declare identity file':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school',
+  replace => true,
+}
