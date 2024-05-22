3efca40cad81cfe21f6d4ee138c16189dd5086d7
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-05-21 19:38:02 -0700

    task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""display all the todo lists of employees tasks done"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
+
+    response = requests.get(url)
+    employeeName = response.json().get('name')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
+
+    for task in tasks:
+        if task.get('completed'):
+            done_tasks.append(task)
+            done += 1
+
+    print(f"Employee {employeeName} is done with tasks({done}/{len(tasks)}):")
+
+    for task in done_tasks:
+        print(f"\t {task.get('title')}")
