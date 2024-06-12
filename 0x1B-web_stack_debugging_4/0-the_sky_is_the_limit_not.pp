c75a28e87ed1185fb7ec727f58555e4aebb7ae18
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-06-12 07:45:51 -0700

    task 1

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,12 @@
 # this puppet file for fix our stack so that we get to 0
 
-exec {'fix--for-nginx':
+exec {'replace':
   provider => shell,
   command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-  before   => Exec['restart-nginxS'],
+  before   => Exec['restart'],
 }
 
-exec {'restart-nginxS':  
+exec {'restart':
   provider => shell,
   command  => 'sudo service nginx restart',
 }
